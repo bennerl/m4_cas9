2398167c278dc8c85629dd49eae3abb7816eef17
Leif Benner <leif.benner@gmail.com>
2016-11-29 10:37:48 -0500

    rf

--- scripts/biowulf.sh
+++ scripts/biowulf.sh
@@ -5,7 +5,7 @@ WD=/data/bennerl/m4_cas9_data/
 rm ${WD}hisat2_swarm.txt
 for i in ${WD}*/
 do
-	rm *sam
+	rm ${i}*sam
 	FILE=${i#$WD}
 	FILE=${FILE%/}
 	echo "hisat2 -x ${ANN}r6.12_dmel_noERCC --rf --dta --known-splicesite-infile ${ANN}r6.12_dmel_noERCC.splicesites.txt --novel-splicesite-outfile ${i}${FILE}_hisat_novel_splicesites.txt -p 8 -1 ${i}*1.fastq -2 ${i}*2.fastq -S ${i}${FILE}.sam" >> ${WD}/hisat2_swarm.txt
