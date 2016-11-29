110cac870d049508465fca2cd931f47fc0765465
Leif Benner <leif.benner@gmail.com>
2016-11-29 10:50:51 -0500

    fr-strand

--- scripts/biowulf.sh
+++ scripts/biowulf.sh
@@ -8,8 +8,10 @@ do
 	rm ${i}*sam
 	FILE=${i#$WD}
 	FILE=${FILE%/}
-	echo "hisat2 -x ${ANN}r6.12_dmel_noERCC --rf --dta --known-splicesite-infile ${ANN}r6.12_dmel_noERCC.splicesites.txt --novel-splicesite-outfile ${i}${FILE}_hisat_novel_splicesites.txt -p 8 -1 ${i}*1.fastq -2 ${i}*2.fastq -S ${i}${FILE}.sam" >> ${WD}/hisat2_swarm.txt
+	echo "hisat2 -x ${ANN}r6.12_dmel_noERCC --fr --rna-strandness RF --dta --known-splicesite-infile ${ANN}r6.12_dmel_noERCC.splicesites.txt --novel-splicesite-outfile ${i}${FILE}_hisat_novel_splicesites.txt -p 8 -1 ${i}*1.fastq -2 ${i}*2.fastq -S ${i}${FILE}.sam" >> ${WD}/hisat2_swarm.txt
 done
+#not --rf
+
 swarm -g 16 -t 8 --time 01:00:00 --module hisat/2.0.4 -f ${WD}hisat2_swarm.txt
 
 
